0309fa0e3e1d4ce87e4f31a50d724080a41ac46e
Robert Magnus <robert.magnus@akquinet.de>
2015-06-23 23:14:13 +0200

    add dependency to s-case-core

---
+++ eu.scasefp7.eclipse.reqeditor/src/eu/scasefp7/eclipse/reqeditor/util/CoreAccess.java
@@ -0,0 +1,13 @@
+package eu.scasefp7.eclipse.reqeditor.util;
+
+import eu.scasefp7.core.ontology.OntologySource;
+
+public class CoreAccess
+{
+
+    public String getOntology(OntologySource source)
+    {
+        return null;
+    }
+
+}
