ae8caeb93f7d6cd8faac75ab0f941aec8cde325d
thdiaman <themisdiamantopoulos@hotmail.com>
2015-08-21 12:54:52 +0300

    Added preference page for the colors of the annotation editor part

---
+++ eu.scasefp7.eclipse.reqeditor/src/eu/scasefp7/eclipse/reqeditor/preferences/DefaultPreferencesInitializer.java
@@ -0,0 +1,30 @@
+package eu.scasefp7.eclipse.reqeditor.preferences;
+
+import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
+import org.eclipse.jface.preference.IPreferenceStore;
+
+import eu.scasefp7.eclipse.reqeditor.Activator;
+
+/**
+ * Provides the default preferences for the requirements editor.
+ * 
+ * @author themis
+ */
+public class DefaultPreferencesInitializer extends AbstractPreferenceInitializer {
+
+	/**
+	 * Initializes this object.
+	 */
+	public DefaultPreferencesInitializer() {
+	}
+
+	@Override
+	public void initializeDefaultPreferences() {
+		IPreferenceStore store = Activator.getDefault().getPreferenceStore();
+		store.setDefault("Entity", "0,0,255");
+		store.setDefault("EntityText", "0,255,255");
+		store.setDefault("IsActorOf", "0,0,255");
+		store.setDefault("ActsOn", "0,0,255");
+		store.setDefault("HasProperty", "0,0,255");
+	}
+}
