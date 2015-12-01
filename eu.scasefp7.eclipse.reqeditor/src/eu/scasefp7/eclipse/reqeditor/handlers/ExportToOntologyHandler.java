c3462f20f78cdb65fe865e7fc2514f18fceae53d
thdiaman <themisdiamantopoulos@hotmail.com>
2015-12-01 13:56:01 +0200

    Added command for exporting all rqs files of a project to ontology

--- eu.scasefp7.eclipse.reqeditor/src/eu/scasefp7/eclipse/reqeditor/handlers/ExportToOntologyHandler.java
+++ eu.scasefp7.eclipse.reqeditor/src/eu/scasefp7/eclipse/reqeditor/handlers/ExportToOntologyHandler.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IFile;
@@ -22,7 +21,7 @@ import eu.scasefp7.eclipse.reqeditor.helpers.RQSHelpers;
  * 
  * @author themis
  */
-public class ExportToOntologyHandler extends AbstractHandler {
+public class ExportToOntologyHandler extends ProjectAwareHandler {
 
 	/**
 	 * This function is called when the user selects the menu item. It reads the selected resource(s) and populates the
@@ -46,77 +45,79 @@ public class ExportToOntologyHandler extends AbstractHandler {
 					}
 				}
 				if (file != null) {
-					// Read the annotations and add them to the ontology
-					ArrayList<String> requirements = RQSHelpers.getRequirements(file);
-					ArrayList<String> annotations = RQSHelpers.getAnnotations(file);
-
 					// Instantiate the static ontology
 					StaticOntologyAPI ontology = new StaticOntologyAPI(file.getProject(), true);
-
-					for (int i = 1; i < requirements.size() + 1; i++) {
-						// Add a new requirement
-						String reqId = "FR" + i;
-						ontology.addRequirement(reqId);
-
-						HashMap<String, String> idmap = new HashMap<String, String>();
-						for (String annotation : annotations) {
-							if (annotation.startsWith(i + ":")) {
-								String Id = annotation.split("\t")[0];
-								String type = Id.split(":")[1].substring(0, 1);
-								if (type.equals("T")) {
-									// Add a class instance in the ontology
-									String anntype = annotation.split("\t")[1].split(" ")[0];
-									String annword = annotation.split("\t")[2];
-									if (anntype.equals("Actor")) {
-										ontology.addActor(annword);
-										ontology.connectRequirementToConcept(reqId, annword);
-									} else if (anntype.equals("Action")) {
-										ontology.addAction(annword);
-										ontology.connectRequirementToOperation(reqId, annword);
-									} else if (anntype.equals("Object")) {
-										ontology.addObject(annword);
-										ontology.connectRequirementToConcept(reqId, annword);
-									} else if (anntype.equals("Property")) {
-										ontology.addProperty(annword);
-										ontology.connectRequirementToConcept(reqId, annword);
-									}
-									idmap.put(Id, annword);
-								}
-							}
-						}
-						for (String annotation : annotations) {
-							if (annotation.startsWith(i + ":")) {
-								String Id = annotation.split("\t")[0];
-								String type = Id.split(":")[1].substring(0, 1);
-								if (type.equals("R")) {
-									// Add a property in the ontology
-									String anninfo = annotation.split("\t")[1];
-									String anntype = anninfo.split(" ")[0];
-									String tann1 = anninfo.split(" ")[1];
-									String tann2 = anninfo.split(" ")[2];
-									String entity1 = idmap.get(tann1);
-									String entity2 = idmap.get(tann2);
-									if (entity1 != null && entity2 != null) {
-										if (anntype.equals("IsActorOf")) {
-											ontology.connectActorToAction(entity1, entity2);
-										} else if (anntype.equals("ActsOn")) {
-											ontology.connectActionToObject(entity1, entity2);
-										} else if (anntype.equals("HasProperty")) {
-											ontology.connectElementToProperty(entity1, entity2);
-										} else if (anntype.equals("RelatesTo")) {
-											ontology.connectObjectToObject(entity1, entity2);
-										}
-									}
-								}
-							}
-						}
-					}
-
-					// Close and save the ontology
+					instantiateOntology(file, ontology);
 					ontology.close();
 				}
 			}
 		}
 		return null;
 	}
+
+	protected void instantiateOntology(IFile file, StaticOntologyAPI ontology) {
+		// Read the annotations and add them to the ontology
+		ArrayList<String> requirements = RQSHelpers.getRequirements(file);
+		ArrayList<String> annotations = RQSHelpers.getAnnotations(file);
+
+		for (int i = 1; i < requirements.size() + 1; i++) {
+			// Add a new requirement
+			String reqId = "FR" + i;
+			ontology.addRequirement(reqId);
+
+			HashMap<String, String> idmap = new HashMap<String, String>();
+			for (String annotation : annotations) {
+				if (annotation.startsWith(i + ":")) {
+					String Id = annotation.split("\t")[0];
+					String type = Id.split(":")[1].substring(0, 1);
+					if (type.equals("T")) {
+						// Add a class instance in the ontology
+						String anntype = annotation.split("\t")[1].split(" ")[0];
+						String annword = annotation.split("\t")[2];
+						if (anntype.equals("Actor")) {
+							ontology.addActor(annword);
+							ontology.connectRequirementToConcept(reqId, annword);
+						} else if (anntype.equals("Action")) {
+							ontology.addAction(annword);
+							ontology.connectRequirementToOperation(reqId, annword);
+						} else if (anntype.equals("Object")) {
+							ontology.addObject(annword);
+							ontology.connectRequirementToConcept(reqId, annword);
+						} else if (anntype.equals("Property")) {
+							ontology.addProperty(annword);
+							ontology.connectRequirementToConcept(reqId, annword);
+						}
+						idmap.put(Id, annword);
+					}
+				}
+			}
+			for (String annotation : annotations) {
+				if (annotation.startsWith(i + ":")) {
+					String Id = annotation.split("\t")[0];
+					String type = Id.split(":")[1].substring(0, 1);
+					if (type.equals("R")) {
+						// Add a property in the ontology
+						String anninfo = annotation.split("\t")[1];
+						String anntype = anninfo.split(" ")[0];
+						String tann1 = anninfo.split(" ")[1];
+						String tann2 = anninfo.split(" ")[2];
+						String entity1 = idmap.get(tann1);
+						String entity2 = idmap.get(tann2);
+						if (entity1 != null && entity2 != null) {
+							if (anntype.equals("IsActorOf")) {
+								ontology.connectActorToAction(entity1, entity2);
+							} else if (anntype.equals("ActsOn")) {
+								ontology.connectActionToObject(entity1, entity2);
+							} else if (anntype.equals("HasProperty")) {
+								ontology.connectElementToProperty(entity1, entity2);
+							} else if (anntype.equals("RelatesTo")) {
+								ontology.connectObjectToObject(entity1, entity2);
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
 }
